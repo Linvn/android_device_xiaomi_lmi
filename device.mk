d123134cd4d515d0ce271df350a66bce900a7d9b
lin <llin1079@gmail.com>
2022-05-07 08:01:31 +0000

    lmi: Build netutils-wrapper-1.0

--- device.mk
+++ device.mk
@@ -321,7 +321,8 @@ PRODUCT_COPY_FILES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor
+    android.system.net.netd@1.1.vendor \
+    netutils-wrapper-1.0
 
 # Neural Networks
 PRODUCT_PACKAGES += \
